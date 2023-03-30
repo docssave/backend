19e43a46861427c64b967e3bad562a24b0c3fc65
Denis Makarenko <denis-makarenko@outlook.com>
2023-03-30 09:03:23 +0300

    Create a table by FluentMigrator

---
+++ src/Services/Identity/Idn.DataAccess/Migrations/20230330_AddUsersTable.cs
@@ -0,0 +1,27 @@
+﻿using FluentMigrator;
+
+namespace Idn.DataAccess.Migrations;
+
+[Migration(202303300824)]
+public sealed class AddUsersTable : Migration
+{
+    public override void Up()
+    {
+        Create.Table("Users")
+            .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+            .WithColumn("Name").AsAnsiString(500).NotNullable()
+            .WithColumn("EncryptedEmail").AsAnsiString(320).NotNullable()
+            .WithColumn("Source").AsAnsiString(12).NotNullable()
+            .WithColumn("SourceUserId").AsAnsiString(36).NotNullable();
+
+        Create.Index("Users_SourceUserId_Index")
+            .OnTable("Users")
+            .OnColumn("SourceUserId").Unique();
+    }
+
+    public override void Down()
+    {
+        Delete.Index("Users_SourceUserId_Index");
+        Delete.Table("Users");
+    }
+}
