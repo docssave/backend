4e842366efc75f372721a4b1b60fab6c9ade7894
Aleksandr Koksharov <56362654+Alex8118@users.noreply.github.com>
2023-04-05 19:49:07 +0300

    I tried to do it but it doesn’t work

--- src/WebApi/Endpoints/TagsEndpoints.cs
+++ src/WebApi/Endpoints/TagsEndpoints.cs
@@ -1,19 +1,28 @@
 ﻿using Idn.Contracts;
+using Idn.DataAccess;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Services;
 
-namespace WebApi.Endpoints;
-
-public static class TagsEndpoints
+namespace WebApi.Endpoints
 {
-    private const string BaseRoute = "api/v1/tags";
-
-    public static void MapTagsEndpoints(this WebApplication application)
+    public static class TagsEndpoints
     {
-        application.MapPut($"{BaseRoute}", CreateAsync);
-    }
+        private const string BaseRoute = "api/v1/tags";
 
-    private static async Task<IResult> CreateAsync([FromServices] IUserAccessor userAccessor)
-    {
-        return Results.Ok();
+        public static void MapTagsEndpoints(this WebApplication application)
+        {
+            application.MapGet($"{BaseRoute}", GetAsync);
+        }
+
+        private static Task<IActionResult> GetAsync([FromServices] IUserAccessor userAccessor)
+        {
+            string[] tags = TagService.GetTag(userAccessor);
+            return Task.FromResult<IActionResult>(Ok(tags)); //tried to write it as return TagService.GetTag(userAccessor) but it doesn't work
+        }
+
+        private static IActionResult Ok(string[] tags) // this method IDE generated
+        {
+            throw new NotImplementedException();
+        }
     }
 }
