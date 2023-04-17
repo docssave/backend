c0e7a711ff545d516a8c2cf993fa8433969dfc50
Denis Makarenko <denis-makarenko@outlook.com>
2023-04-17 09:01:36 +0300

    Added all needed stuff for new service

---
+++ src/Services/WorkSpace/Ws.DataAccess/SqlQueries.cs
@@ -0,0 +1,20 @@
+﻿using Idn.Contracts;
+using SqlKata;
+
+namespace Ws.DataAccess;
+
+internal static class SqlQueries
+{
+    public static Query CreateWorkspaceQuery(string name) => new Query("Workspaces")
+        .AsInsert(new
+        {
+            Name = name
+        }, returnId: true);
+
+    public static Query CreateUserWorkspaceQuery(UserId userId, long workspaceId) => new Query("UserWorkspaces")
+        .AsInsert(new
+        {
+            UserId = userId,
+            WorkspaceId = workspaceId
+        });
+}
