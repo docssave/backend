830896c7ec47a4a2625787cde87d827f859fc812
Denis Makarenko <denis-makarenko@outlook.com>
2023-04-18 08:04:29 +0300

    Filled DataAccess layer

--- src/Libraries/Sql/SqlServer.Abstraction/Extensions/DbConnectionFactoryExtensions.cs
+++ src/Libraries/Sql/SqlServer.Abstraction/Extensions/DbConnectionFactoryExtensions.cs
@@ -17,4 +17,28 @@ public static class DbConnectionFactoryExtensions
             return exceptionFunc(e);
         }
     }
+
+    public static async Task<T> TryAsync<T>(
+        this IDbConnectionFactory dbConnectionFactory,
+        Func<IDbConnection, IDbTransaction, Task<T>> sqlFunc,
+        Func<Exception, T> exceptionFunc) =>
+        await dbConnectionFactory.TryAsync(async connection =>
+        {
+            var transaction = connection.BeginTransaction();
+
+            try
+            {
+                var result = await sqlFunc(connection, transaction);
+                
+                transaction.Commit();
+ 
+                return result;
+            }
+            catch (Exception e)
+            {
+                transaction.Rollback();
+
+                throw e;
+            }
+        }, exceptionFunc);
 }
--- src/Services/WorkSpace/Ws.DataAccess/SqlQueries.cs
+++ src/Services/WorkSpace/Ws.DataAccess/SqlQueries.cs
@@ -5,16 +5,24 @@ namespace Ws.DataAccess;
 
 internal static class SqlQueries
 {
-    public static Query CreateWorkspaceQuery(string name) => new Query("Workspaces")
-        .AsInsert(new
-        {
-            Name = name
-        }, returnId: true);
+    public static Query CreateWorkspaceQuery(string name) =>
+        new Query("Workspaces")
+            .AsInsert(new
+            {
+                Name = name
+            }, returnId: true);
 
-    public static Query CreateUserWorkspaceQuery(UserId userId, long workspaceId) => new Query("UserWorkspaces")
-        .AsInsert(new
-        {
-            UserId = userId,
-            WorkspaceId = workspaceId
-        });
+    public static Query CreateUserWorkspaceQuery(UserId userId, long workspaceId) =>
+        new Query("UserWorkspaces")
+            .AsInsert(new
+            {
+                UserId = userId,
+                WorkspaceId = workspaceId
+            });
+
+    public static Query GetWorkspaceQuery(UserId userId) =>
+        new Query("Workspaces w")
+            .Select("w.Id", "w.Name")
+            .Join("UserWorkspaces uw", "uw.WorkspaceId", "w.Id")
+            .Where("uw.UserId", userId);
 }
