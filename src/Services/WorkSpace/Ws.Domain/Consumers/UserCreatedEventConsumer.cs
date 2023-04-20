966ce7cf5e06cef381a258a210c531c030364aa1
Denis Makarenko <denis-makarenko@outlook.com>
2023-04-20 07:58:50 +0300

    Added handler for notification

---
+++ src/Services/WorkSpace/Ws.Domain/Consumers/UserCreatedEventConsumer.cs
@@ -0,0 +1,25 @@
+﻿using Idn.Contracts.Events;
+using MediatR;
+using Ws.DataAccess;
+
+namespace Ws.Domain.Consumers;
+
+internal sealed class UserCreatedEventConsumer : INotificationHandler<UserCreatedEvent>
+{
+    private readonly IWorkspaceRepository _repository;
+
+    public UserCreatedEventConsumer(IWorkspaceRepository repository) =>
+        _repository = repository;
+    
+    public async Task Handle(UserCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        var result = await _repository.GetWorkspaceAsync(notification.Id);
+
+        if (result.Value is not Workspace)
+        {
+            const string defaultName = "workspace1";
+
+            await _repository.CreateWorkspaceAsync(notification.Id, defaultName);
+        }
+    }
+}
