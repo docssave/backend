5ec909f8a573d7bd594eeb15720797ba738ad7d8
Denis Makarenko <denis-makarenko@outlook.com>
2023-05-27 07:52:02 +0300

    Filled migrations

--- src/Services/Collection/Col.DataAccess/Migrations/20230508_AddUserCollectionTable.cs
+++ src/Services/Collection/Col.DataAccess/Migrations/20230508_AddUserCollectionTable.cs
@@ -1,7 +1,23 @@
+using FluentMigrator;
+
 namespace Col.DataAccess.Migrations;
 
 [Migration(20230508)]
 public class AddUserCollectionTable : Migration
 {
-    
+    public override void Up()
+    {
+        Create.Table("UserCollections")
+            .WithColumn("UserId").AsInt64()
+            .WithColumn("CollectionId").AsGuid();
+
+        Create.PrimaryKey("UserCollections_PrimaryKey")
+            .OnTable("UserCollections")
+            .Columns("UserId", "CollectionId");
+    }
+
+    public override void Down()
+    {
+        Delete.Table("UserCollections");
+    }
 }
