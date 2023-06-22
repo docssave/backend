e61db6ce3a251ab6f2da90b7790562a3c80785ff
Denis Makarenko <denis-makarenko@outlook.com>
2023-06-22 06:57:52 +0300

    Added queries + method for repository

--- src/Services/Collection/Col.DataAccess/CollectionRepository.cs
+++ src/Services/Collection/Col.DataAccess/CollectionRepository.cs
@@ -1,23 +1,37 @@
 using Col.Contracts;
+using Dapper;
 using Idn.Contracts;
 using SqlServer.Abstraction;
+using SqlServer.Abstraction.Extensions;
 
 namespace Col.DataAccess;
 
-public sealed class CollectionRepository : ICollectionRepository
+public sealed class CollectionRepository : RepositoryBase, ICollectionRepository
 {
     private readonly IDbConnectionFactory _connectionFactory;
 
     public CollectionRepository(IDbConnectionFactory connectionFactory) => 
         _connectionFactory = connectionFactory;
-    
-    public Task<IReadOnlyList<Collection>> ListCollectionsAsync(UserId userId)
+
+    public Task<IReadOnlyList<Collection>> ListCollectionsAsync(UserId userId) => _connectionFactory.TryAsync(async connection =>
     {
-        throw new NotImplementedException();
-    }
+        var sqlQuery = QueryCompiler.ToSqlQueryString(SqlQueries.GetCollectionsQuery(userId));
+
+        var entities = await connection.QueryAsync<CollectionEntity>(sqlQuery);
+
+        return entities.Select(entity => new Collection(
+                new CollectionId(entity.Id),
+                entity.Name,
+                entity.Icon,
+                Enum.Parse<EncryptSide>(entity.EncryptSide),
+                entity.Version))
+            .ToList();
+    }, RepositoryResult<IReadOnlyList<Collection>>.Failed);
 
     public Task<Collection> RegisterCollectionAsync(CollectionId id, string name, string icon, EncryptSide encryptSide, int version)
     {
         throw new NotImplementedException();
     }
+
+    private sealed record CollectionEntity(Guid Id, string Name, string Icon, string EncryptSide, int Version);
 }
--- src/Services/Collection/Col.DataAccess/SqlQueries.cs
+++ src/Services/Collection/Col.DataAccess/SqlQueries.cs
@@ -1,10 +1,12 @@
 using Col.Contracts;
+using Idn.Contracts;
 using SqlKata;
 
 namespace Col.DataAccess;
 
 internal static class SqlQueries
 {
+    //TODO: SQL Server doesn't support ON DUPLICATE KYE
     public static Query RegisterCollectionQuery(CollectionId id, string name, string icon, EncryptSide encryptSide, int version) =>
         new Query("Collections")
             .AsInsert(new
@@ -14,5 +16,20 @@ internal static class SqlQueries
                 Icon = icon,
                 EncryptSide = encryptSide,
                 Version = version
-            }).;
+            });
+
+    public static Query RegisterUserCollectionQuery(UserId userId, CollectionId collectionId) =>
+        new Query("UserCollections")
+            .AsInsert(new
+            {
+                UserId = userId,
+                CollectionId = collectionId
+            });
+
+    public static Query GetCollectionsQuery(UserId userId) =>
+        new Query("Collections")
+            .Select("Id", "Name", "Icon", "EncryptSide", "Version")
+            .Join("UserCollections", "UserCollections.CollectionId", "Collections.Id")
+            .Where("UserCollections.UserId", userId);
+
 }
