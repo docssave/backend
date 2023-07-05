4d4c1a585d91ec970f14e554f2b604622ee79c15
Aleksandr Koksharov <56362654+Alex8118@users.noreply.github.com>
2023-07-05 08:47:01 +0300

    Add Tag Extensions

---
+++ src/Services/Tag/Tag.Extensions/ServiceTagExtensions.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tag.Extensions;
+
+public static class ServiceTagExtensions
+{
+    
+    public static IServiceTag AddWorkspaceServiceTag(this IServiceTag services)
+        {
+            return services
+                .AddMediatR(typeof(MediatorEntryPoint).Assembly)
+                .AddWorkspace(services)
+                .BuildServiceProvider();
+        }
+    
+}
