ed408f40825e7531b2151f5550df0e3b910d5188
Aleksandr Koksharov <56362654+Alex8118@users.noreply.github.com>
2023-07-07 08:30:59 +0300

    Service tag extensions

---
+++ src/Services/Tag/Tag.Domain/ServiceTagExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tag.Domain;
+
+public static class ServiceTagExtensions
+{
+    public static IServiceTag AddIdentity(this IServiceTag services)
+    {
+        return services;
+    }
+    
+}
