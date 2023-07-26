2328379301bd5af3931093934158620c94ec9f69
Denis Makarenko <denis-makarenko@outlook.com>
2023-07-26 06:51:36 +0300

    Added Request and Handler

---
+++ src/Services/Collection/Col.Domain/V1/Handlers/RegisterCollectionHandler.cs
@@ -0,0 +1,14 @@
+﻿using Col.Contracts.V1;
+using MediatR;
+using OneOf;
+using OneOf.Types;
+
+namespace Col.Domain.V1.Handlers;
+
+internal sealed class RegisterCollectionHandler : IRequestHandler<RegisterCollectionRequest, OneOf<Collection, Error<string>>>
+{
+    public Task<OneOf<Collection, Error<string>>> Handle(RegisterCollectionRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
