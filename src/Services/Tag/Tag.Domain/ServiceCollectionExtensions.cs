3325e25e1958495823f5747b3e6763b6cc83713c
Aleksandr Koksharov <56362654+Alex8118@users.noreply.github.com>
2023-09-23 09:23:57 +0300

    added validator

---
+++ src/Services/Tag/Tag.Domain/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using TagContracts;
+
+namespace Tag.Domain;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddTags(this IServiceCollection services)
+    {
+        services.AddSingleton<IValidator<CreateTagRequest>, CreateTagRequestValidator>();
+        return services;
+    }
+}
