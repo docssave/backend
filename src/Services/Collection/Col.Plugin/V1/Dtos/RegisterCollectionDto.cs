f00368e399468a9e4ff37b53890ba8291e37ae6f
Denis Makarenko <denis-makarenko@outlook.com>
2023-10-02 07:40:01 +0300

    Added a List handler

---
+++ src/Services/Collection/Col.Plugin/V1/Dtos/RegisterCollectionDto.cs
@@ -0,0 +1,5 @@
+﻿using Col.Contracts.V1;
+
+namespace Col.Plugin.V1.Dtos;
+
+public sealed record RegisterCollectionDto(string Name, string Icon, EncryptionSide EncryptionSide, int? Version);
--- src/Services/Collection/Col.Plugin/V1/Extensions/WebApplicationExtensions.cs
+++ src/Services/Collection/Col.Plugin/V1/Extensions/WebApplicationExtensions.cs
@@ -1,8 +1,12 @@
-﻿using Col.Contracts.V1;
+﻿using Badger.Plugin.Filters;
+using Col.Contracts.V1;
+using Col.Plugin.V1.Dtos;
+using Col.Plugin.V1.Validators;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 
 namespace Col.Plugin.V1.Extensions;
 
@@ -12,12 +16,25 @@ public static class WebApplicationExtensions
 
     public static void UseCollection(this WebApplication application)
     {
-        application.MapPut(BaseRoute, RegisterCollectionAsync);
+        application.MapGroup(BaseRoute).UseCollectionsEndpoints();
+    }
+    
+    private static void UseCollectionsEndpoints(this IEndpointRouteBuilder group)
+    {
+        group.MapPut("/{collectionId}", RegisterCollectionAsync).AddEndpointFilter<ValidationFilter<RegisterCollectionDto>>();
+        group.MapGet("/", ListCollectionsAsync);
     }
 
-    private static async Task<IResult> RegisterCollectionAsync(RegisterCollectionRequest request, [FromServices] IMediator mediator)
+    private static async Task<IResult> RegisterCollectionAsync(RegisterCollectionDto request, [FromServices] IMediator mediator)
     {
         var response = await mediator.Send(request);
+
+        return response.Match(Results.Ok, Results.BadRequest);
+    }
+
+    private static async Task<IResult> ListCollectionsAsync([FromServices] IMediator mediator)
+    {
+        var response = await mediator.Send(new ListCollectionsRequest());
 
         return response.Match(Results.Ok, Results.BadRequest);
     }
