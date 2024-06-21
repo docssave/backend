006936edb8abd224644a9552ac33481ea0648b5b
Denis Makarenko <denmaklucky@users.noreply.github.com>
2024-06-21 18:33:59 +0200

    feat: add enpoint for getting all files in the document

--- src/Services/Document/Doc.Domain/V1/Handlers/ListFilesHandler.cs
+++ src/Services/Document/Doc.Domain/V1/Handlers/ListFilesHandler.cs
@@ -1,6 +1,8 @@
 ﻿using Badger.OneOf.Types;
+using Col.Contracts.V1;
 using Doc.Contracts.V1;
 using Doc.Domain.V1.DataAccess;
+using Idn.Contracts.V1;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using OneOf;
@@ -9,27 +11,38 @@ using File = Doc.Contracts.V1.File;
 
 namespace Doc.Domain.V1.Handlers;
 
-internal sealed class ListFilesHandler(IFileRepository repository, ILogger<ListFilesHandler> logger)
-    : IRequestHandler<ListFilesRequest, OneOf<IReadOnlyList<File>, Error<string>>>
+internal sealed class ListFilesHandler(
+    IFileRepository repository,
+    ISender mediator,
+    IUserIdAccessor userIdAccessor,
+    ILogger<ListFilesHandler> logger)
+    : IRequestHandler<ListFilesRequest, OneOf<IReadOnlyList<File>, Unknown, Forbidden, Unreachable>>
 {
-    public async Task<OneOf<IReadOnlyList<File>, Error<string>>> Handle(ListFilesRequest request, CancellationToken cancellationToken)
+    public async Task<OneOf<IReadOnlyList<File>, Unknown, Forbidden, Unreachable>> Handle(ListFilesRequest request, CancellationToken cancellationToken)
     {
-        if (request.DocumentId == DocumentId.Empty)
-        {
-            logger.LogError("DocumentId can not be empty");
+        var userId = userIdAccessor.UserId;
 
-            return new Error<string>("Could not find files for empty document.");
+        if (userId == null)
+        {
+            logger.LogError("Could not get UserId from IUserIdAccessor");
+            return new Unknown();
+        }
+        
+        var checkAccess = await mediator.Send(new CollectionAccessRequest(userId.Value, request.CollectionId), cancellationToken);
+
+        if (checkAccess.IsT1)
+        {
+            return new Forbidden();
         }
 
         var result = await repository.ListAsync(request.DocumentId);
 
-        return result.MapT1(ToError);
+        return result.Match(OneOf<IReadOnlyList<File>, Unknown, Forbidden, Unreachable>.FromT0, ToError);
 
-        Error<string> ToError(Unreachable<string> error)
+        OneOf<IReadOnlyList<File>, Unknown, Forbidden, Unreachable> ToError(Unreachable<string> error)
         {
             logger.LogError("Could not reach `{Repository}` with the reason: {Reason}", nameof(IFileRepository), error.Value);
-
-            return new(error.Value);
+            return new();
         }
     }
 }
